9d31c969411c635e7820f7e6ab2f2259f9d9dd72
haniehbabaee <70447525+haniehbabaee@users.noreply.github.com>
2021-01-12 18:13:28 -0500

    update app.js

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -1,6 +1,7 @@
 var tableData = data;
 var tbody = d3.select("tbody");
 
+
 tableData.forEach(function(record) {
     var row = tbody.append("tr");
     Object.entries(record).forEach(function([key, value]){
@@ -8,3 +9,35 @@ tableData.forEach(function(record) {
         cell.text(value);
     });
 });
+
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-group");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    
+    var inputValue = d3.select("#datetime").property("value");
+   
+    var filteredData = tableData.filter(record => record.datetime ===inputValue);
+    //console.log(filteredData);
+    tbody.html(" ");
+    
+    filteredData.forEach((dataRow) => {
+        var row = tbody.append("tr");
+        Object.entries(dataRow).forEach(function([key,value]){
+            var cell=row.append("td")
+            cell.text(value)
+        })
+    });
+
+
+
+    // Object.entries(filteredData).forEach(function([key, value]){
+    //     console.log(value);
+    //     var cell =row.append("td");
+    //     cell.text(value);
+    // });
+};
