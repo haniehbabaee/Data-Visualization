9fa455fbfc26b3e492f802fd3422077184b578b9
haniehbabaee <70447525+haniehbabaee@users.noreply.github.com>
2021-01-13 20:46:53 -0500

    update

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -22,22 +22,14 @@ function runEnter() {
     var inputValue = d3.select("#datetime").property("value");
    
     var filteredData = tableData.filter(record => record.datetime ===inputValue);
-    //console.log(filteredData);
+    
     tbody.html(" ");
     
     filteredData.forEach((dataRow) => {
         var row = tbody.append("tr");
         Object.entries(dataRow).forEach(function([key,value]){
-            var cell=row.append("td")
+            var cell = row.append("td")
             cell.text(value)
         })
     });
-
-
-
-    // Object.entries(filteredData).forEach(function([key, value]){
-    //     console.log(value);
-    //     var cell =row.append("td");
-    //     cell.text(value);
-    // });
 };
---
+++ javascript-challenge/UFO-level-2/static/js/app.js
@@ -0,0 +1,52 @@
+var tableData = data;
+var tbody = d3.select("tbody");
+
+
+tableData.forEach(function(record) {
+    var row = tbody.append("tr");
+    Object.entries(record).forEach(function([key, value]){
+        var cell =row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.selectAll("#filter-btn");
+var form = d3.selectAll(".form-control");
+
+//button.on("click", runEnter);
+//form.on("submit", runEnter);
+form.on('change',runEnter)
+
+function runEnter() {
+    d3.event.preventDefault();
+   
+    var inputID = d3.select(this).attr('id')
+    console.log(inputID)
+    var inputValue = d3.select(this).property('value')
+    console.log(inputValue)
+  
+    if (inputID ==="datetime"){
+        var filteredData = tableData.filter(record => record.datetime ===inputValue);
+    }
+    else if (inputID ==="city"){
+        var filteredData = tableData.filter(record => record.city ===inputValue);
+    }
+    else if (inputID ==="state"){
+        var filteredData = tableData.filter(record => record.state ===inputValue);
+    }
+    else if (inputID ==="country"){
+        var filteredData = tableData.filter(record => record.country ===inputValue);
+    }
+    else {
+        var filteredData = tableData.filter(record => record.shape ===inputValue);
+    }
+    tbody.html(" ");
+    
+    filteredData.forEach((dataRow) => {
+        var row = tbody.append("tr");
+        Object.entries(dataRow).forEach(function([key,value]){
+            var cell = row.append("td")
+            cell.text(value)
+        })
+    });
+};
